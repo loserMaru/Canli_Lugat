597d08ea5fb8b908396196e2a04ae55140545737
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2023-09-21 13:39:19 +0300

    Blueprint route for home page

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-import re
-
-from flask import Flask, render_template
+from flask import Flask
 
 from config import Config
 from extensions.database_extension import db
-from models.words import Word
+from routes.home import home_bp
 
 
 def create_app():
@@ -13,53 +11,13 @@ def create_app():
     app.config.from_object(Config)
     # Inits
     db.init_app(app)
+
+    # Blueprints
+    app.register_blueprint(home_bp)
     return app
 
 
 app = create_app()
-
-
-@app.route('/')
-def index():
-    book = """Макаронлар, лезетли! малинаны. марлядан мархагъа мансурлы макъас одфоывд"""
-    words_and_punctuation = re.findall(r'\w+|\W+', book)
-
-    counter = 0
-    book_words = []
-    for item in words_and_punctuation:
-        if item.isalpha():
-            book_words.append([counter, item])
-            counter += 1
-        else:
-            book_words.append([-1, item.replace(' ', '&nbsp;')])
-
-    print(book_words)
-
-    a = (book
-         .replace(',', '')
-         .replace('!', '')
-         .replace(':', '')
-         .replace('.', ''))
-    words = a.split()
-    translations = {}
-
-    for i, word in enumerate(words):
-        db_words = Word.query.filter(Word.word.ilike(f'{word}%'))
-        if db_words.count() == 0:
-            for suffix in ["дан", "лер", "гъа", "лар", "лы", "ли", "ны", "ни", "макъ", "мек", "къ"]:
-                if word.endswith(suffix):
-                    inf = word[:-len(suffix)]
-                    db_words = Word.query.filter(Word.word.ilike(f'{inf}'))
-                    break
-            else:
-                continue
-
-        for db_word in db_words:
-            print(f'{word} => {db_word.word} - {db_word.translation}')
-            translations[i] = (db_word.word, db_word.translation)
-
-    return render_template('index.html', translations=translations, words=book_words)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ routes/home.py
@@ -0,0 +1,50 @@
+import re
+
+from flask import Blueprint, render_template
+
+from models.words import Word
+
+home_bp = Blueprint('home', __name__, template_folder='templates')
+
+
+@home_bp.route('/')
+def index():
+    book = """Макаронлар, лезетли! малинаны. марлядан мархагъа мансурлы макъас одфоывд"""
+    words_and_punctuation = re.findall(r'\w+|\W+', book)
+
+    counter = 0
+    book_words = []
+    for item in words_and_punctuation:
+        if item.isalpha():
+            book_words.append([counter, item])
+            counter += 1
+        else:
+            book_words.append([-1, item.replace(' ', '&nbsp;')])
+
+    print(book_words)
+
+    a = (book
+         .replace(',', '')
+         .replace('!', '')
+         .replace(':', '')
+         .replace('.', ''))
+    words = a.split()
+    translations = {}
+
+    for i, word in enumerate(words):
+        db_words = Word.query.filter(Word.word.ilike(f'{word}%'))
+        if db_words.count() == 0:
+            for suffix in ["дан", "тан", "лар", "лер", "гъа", "къа", "лы", "ли", "ны", "ни", "макъ", "мек", "гъы",
+                           "къ"]:
+                if word.endswith(suffix):
+                    inf = word[:-len(suffix)]
+                    db_words = Word.query.filter(Word.word.ilike(f'{inf}'))
+                    break
+            else:
+                continue
+
+        for db_word in db_words:
+            print(f'{word} => {db_word.word} - {db_word.translation}')
+            translations[i] = (db_word.word, db_word.translation)
+
+    return render_template('index.html', translations=translations, words=book_words)
